6538f9ed1abcce2731719ba65598f8478b2709a0
instalilian888-boop <instalilian888@gmail.com>
2026-04-30 09:36:05 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function mostrarDatos(){
   let nombre = document.getElementById("nombre").value.trim();
   let edad = document.getElementById("edad").value.trim();
-   let correo = document.getElementById("correo").value.trim();
+  let correo = document.getElementById("correo").value.trim();
   let grupo = document.getElementById("grupo").value.trim();
-   let  telefono = document.getElementById("telefono").value.trim();
-   let comentario= document.getElementById("comentario").value.trim();
+  let telefono = document.getElementById("telefono").value.trim();
+  let comentario= document.getElementById("comentario").value.trim();
   let resultado = document.getElementById("resultado");
 
   if(nombre === "" || edad === ""){
@@ -15,11 +15,11 @@ function mostrarDatos(){
     resultado.innerHTML =
       "<strong>✅ Datos capturados correctamente:</strong><br><br>" +
       "Nombre: " + nombre + "<br>" +
-      "Edad: " + edad + " años";
-      "Correo" + correo + "<br>" +
-      "Grupo: " + grupo + " grupo";
-      "Telefono: " + telefono + " numeros ";
-      "Comentario: " + comentario + "<br>" +
+      "Edad: " + edad + " años<br>" +
+      "Correo: " + correo + "<br>" +
+      "Grupo: " + grupo + "<br>" +
+      "Telefono: " + telefono + "<br>" +
+      "Comentario: " + comentario;
 
     resultado.style.background = "#e8ffe8";
     resultado.style.border = "2px solid #2ecc71";
